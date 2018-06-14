d8033807f4225666e39ee11823e1c2f8873adc7b
ankit.vajani <ankit.v@simformsolutions.com>
2018-06-14 12:35:20 +0530

    migration changes

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -24,6 +24,10 @@ class CreateUsersTable extends Migration
             $table->unsignedBigInteger('facebook_id')->nullable()->unique()->index();
             $table->boolean('is_active')->default(true);
             $table->string('remember_token')->nullable()->index();
+            $table->enum('device_type', [0, 1])->default(0)->index();
+            $table->string('push_token')->nullable()->index();
+            $table->string('time_zone', 10)->index()->nullable();
+            $table->timestamp('last_login_at')->nullable()->index();
             $table->timestamps();
         });
     }
